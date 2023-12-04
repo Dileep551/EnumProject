b128badb0a62966913a7bf819da88ecd9bc66703
e146508 <Dileep.Kessireddy@wnco.com>
2023-12-04 17:33:15 -0600

    Added enum Toppings

--- src/Main.java
+++ src/Main.java
@@ -14,19 +14,44 @@ public class Main {
 
             weekDay = getRandomDay();
 
-            System.out.printf("Name is %s, Ordinal value = %d%n", weekDay.name(), weekDay.ordinal());
+            switchDayOfWeek(weekDay);
 
-            if(weekDay == DayOfTheWeek.FRI){
-                System.out.println("Found Friday");
-            }
+//            System.out.printf("Name is %s, Ordinal value = %d%n", weekDay.name(), weekDay.ordinal());
+//
+//            if(weekDay == DayOfTheWeek.FRI){
+//                System.out.println("Found Friday");
+//            }
         }
 
+        for (Topping topping : Topping.values()) {
+
+            System.out.println(topping.name() +" "+topping.getPrice());
+        }
+
+    }
+
+    public static void switchDayOfWeek(DayOfTheWeek weekDay){
+
+        int weekDayInteger = weekDay.ordinal() + 1;
+
+        System.out.print("-".repeat(30));
+
+        System.out.println("weekDayInteger" + weekDayInteger);
+
+        switch (weekDay){
+            case WED -> System.out.println("Wednesday is the Day" + weekDayInteger);
+            case SAT -> System.out.println("Saturday is the Day" + weekDayInteger);
+            default -> System.out.println(weekDay.name().charAt(0) + weekDay.name().substring(1).toLowerCase() +
+                    "day is Day " + weekDayInteger);
+        }
     }
 
     public static DayOfTheWeek getRandomDay(){
 
         int randomInteger = new Random().nextInt(7);
         var allDays = DayOfTheWeek.values();
+
+        System.out.println("The random number I got is & Day " + randomInteger +" "+ allDays[randomInteger]);
 
         return allDays[randomInteger];
 
